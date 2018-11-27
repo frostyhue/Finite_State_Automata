d18b8666688ad7eda6f432e7dbb22de9ba6bf47c
Tinchev,Rostislav R.I <I358097@fhict.nl>
2018-11-27 13:39:05 +0100

    ADD functionality to parser to add final states to list, all 3 tests are successful

--- tests/Parser.py
+++ tests/Parser.py
@@ -15,6 +15,7 @@ class Parser(object):
         self.current_token = self.tokens_list[0]
         self.alphabet = []
         self.states = []
+        self.final = []
 
     # Method that raises and error.
     def error(self):
@@ -47,5 +48,15 @@ class Parser(object):
                     self.pop_token(LETTER_CAPITAL)
                     if self.current_token.type == COMMA:
                         self.pop_token(COMMA)
+            elif token.value == 'final:':
+                self.pop_token(RESERVED)
+                while self.current_token.type == LETTER_CAPITAL:
+                    self.final.append(self.current_token)
+                    if self.lexer.expr_end():
+                        break
+                    self.pop_token(LETTER_CAPITAL)
+                    if self.current_token.type == COMMA:
+                        self.pop_token(COMMA)
+            
         else:
             print('Unexpected type!')
