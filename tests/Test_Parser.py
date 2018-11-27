7ed790cec35a731ac0791aba4af72704beaed045
Tinchev,Rostislav R.I <I358097@fhict.nl>
2018-11-27 13:23:53 +0100

    ADD test if parser assigns states to states list

--- tests/Test_Parser.py
+++ tests/Test_Parser.py
@@ -15,6 +15,17 @@ class TestLexer(unittest.TestCase):
 
         self.assertEqual(alphabet_test_expected, alphabet_test_output)
 
+    def test_parser_if_assigns_states(self):
+        states_test_value = 'states: Z, A, B'
+        states_test_lexer = Lexer(states_test_value)
+        states_test_parser = Parser(states_test_lexer)
+        states_test_parser.process_statement()
+        states_test_output = states_test_parser.states
+
+        states_test_expected = [Token('Z', LETTER_CAPITAL), Token('A', LETTER_CAPITAL), Token('B', LETTER_CAPITAL),]
+
+        self.assertEqual(states_test_expected, states_test_output)
+
 
 if __name__ == '__main__':
     unittest.main()
